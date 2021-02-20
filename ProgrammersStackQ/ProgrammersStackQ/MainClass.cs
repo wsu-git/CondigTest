ba00d4365ec93b31acac5e9bc235283f6614a974
wsu-git <banguk93@gmail.com>
2021-02-21 02:36:29 +0900

    주식 가격 추가.

---
+++ ProgrammersStackQ/ProgrammersStackQ/MainClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ProgrammersStackQ
+{
+    public class MainClass
+    {
+        public MainClass()
+        {
+        }
+    }
+}
---
+++ ProgrammersStackQ/ProgrammersStackQ/StockPrice.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ProgrammersStackQ
+{
+    public class StockPrice
+    {
+        public StockPrice()
+        {
+        }
+    }
+}
